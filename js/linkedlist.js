41c5c7d8a849105eddc220d7864cdea0804c63c8
samuelclerod <samuelclerod@gmail.com>
2018-03-22 10:30:06 -0300

    Incluido corpo da lista simplesmente encadeada

---
+++ js/linkedlist.js
@@ -0,0 +1,17 @@
+function Node(element){
+    this.element=element;
+    this.next=null;
+}
+function LinkedList(){
+    var length =0;
+    var head = null;
+    this.show = function(separator=', '){}
+    this.append = function(element){}
+    this.insert = function(position, element){}
+    this.removeAt = function(position){} 
+    this.remove = function(element){}
+    this.indexOf = function(element){}
+    this.isEmpty = function(){}
+    this.size = function(){}
+    this.getElement = function (position){}
+}
