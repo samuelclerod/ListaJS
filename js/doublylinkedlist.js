79a4331f062eeee10e18a371ba83b2363f31b4ed
samuelclerod <samuelclerod@gmail.com>
2018-04-12 08:05:12 -0300

    Implementada a lista duplamente encadeada

--- js/doublylinkedlist.js
+++ js/doublylinkedlist.js
@@ -24,23 +24,30 @@ class DoublyLinkedList extends LinkedList{
             let node = new Node(element),
                 current = this.head,
                 index = 0;
-                if(position===0){//será inserido na posição 0
-                    if(!this.head){
-                        this.head=node;
-                        this.tail=node;
-                    }else{
-                        node.next = this.head;
-                        current.previous = node;
-                        this.head=node;
-                    }
-                }else if(position===this.length){//inserido fim
-                    current=this.tail;
-                    current.next=node;
-                    node.previous=current;
+            if(position===0){//será inserido na posição 0
+                if(!this.head){
+                    this.head=node;
                     this.tail=node;
-                }else{//inserido no meio
-                    ///...
+                }else{
+                    node.next = this.head;
+                    current.previous = node;
+                    this.head=node;
                 }
+            }else if(position===this.length){//inserido fim
+                current=this.tail;
+                current.next=node;
+                node.previous=current;
+                this.tail=node;
+            }else{//inserido no meio
+                while(index<position){
+                    index++;
+                    current=current.next;
+                }
+                current.previous.next=node;
+                node.previous=current.previous;
+                current.previous=node;
+                node.next=current;
+            }
             this.length++;
             return true;
         }else{
@@ -49,6 +56,38 @@ class DoublyLinkedList extends LinkedList{
         
     }
 
-    removeAt(position){}
+    removeAt(position){
+        if (position>=0 && position<this.length) {
+            let current=this.head,
+                index=0;
+            if(position===0){ //é no inicio/head
+                this.head=current.next;
+                if(this.length===1){//a lista só tinha um elemento
+                    this.tail=null;
+                }else{// a lista tinha mais de um elemento
+                    current.next==null;
+                    this.head.previous=null;
+                }
+            }else if(position===(this.length-1)){ //é no fim/tail
+                current=this.tail;
+                this.tail= current.previous;
+                current.previous=null;
+                this.tail.next=null;
+            }else{// é no meio
+                while(index<position){
+                    index++;
+                    current=current.next;
+                }
+                current.previous.next=current.next;
+                current.next.previous=current.previous;
+                current.previous=current.next=null;
+            }
+            this.length--;
+            return current.element;
+        }else{
+            return null;
+        }
+
+    }
 
 }
