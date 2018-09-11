54038db29bc3dc18f8da47bdb1a97408ecf70b63
samuelclerod <samuelclerod@gmail.com>
2018-09-10 21:47:59 -0300

    finalizada a implementação da lista encadeada

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -30,13 +30,84 @@ class LinkedList {
         this.lenght++;
     }
 
-    insert(position, element) { }
+    insert(position, element) { 
+        if(position>=0 && position<=this.lenght){
+            let node = new Node(element),
+                current = this.head,
+                index = 0,
+                previous = null;
+            if(position===0){//primeiro posição
+                node.next = current;
+                this.head = node;  
+            }else{// depois da primeira
+                while(index<position){
+                    index++;
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            this.lenght++
+            return true; 
+        }else{
+            return false;
+        }
+    }
 
-    remove(element) { }
+    remove(element) {
+        let index = this.indexOf(element);
+        this.removeAt(index);
+     }
 
-    removeAt(position) { }
+    removeAt(position) {
+        if(position>=0 && position<this.lenght){
+            let current = this.head,
+                previous = null,
+                index = 0;
+            if(position===0){
+                this.head = current.next;
+                current.next = null;
+                this.lenght--;
+            }else{
+                while(index<position){
+                    index++;
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+                current.next = null;
+                this.lenght--;
+            }
+            return current.element;
+        } else{
+            return null
+        }
+    }
 
-    indexOf(element) { }
+    indexOf(element) {
+        let current = this.head,
+            index = 0;
+        while(current){
+            if(element===current.element)
+                return index;
+            index++
+            current = current.next;
+        }
+        return -1;
+    }
+
+    getElement(position) {
+        let current = this.head,
+            index = 0;
+        while (current){
+            if(index===position)
+                return current.element;
+            index++
+            current = current.next;
+        }
+        return null;
+     }
 
     isEmpty() {
         return this.head == null;
@@ -45,6 +116,4 @@ class LinkedList {
     size() {
         return this.lenght;
     }
-
-    getElement(position) { }
 }
