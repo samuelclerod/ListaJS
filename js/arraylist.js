5c5cd7d256d26dd76959c2fcc56526e27ce82aaf
samuelclerod <samuelclerod@gmail.com>
2019-04-04 21:51:23 -0300

    implementada a lista estática

--- js/arraylist.js
+++ js/arraylist.js
@@ -9,29 +9,46 @@ class ArrayList{
     }
     
     append(element){
+        this.data.push(element);
     }
 
     insert(position, element){
+        if(position>-1 && position<=this.size())
+            this.data.splice(position,0,element);
     }
     
     removeAt(position){
+        if (position > -1 && position < this.size())
+            this.data.splice(position,1);
     }
     
     remove(element){
+        let index = this.indexOf(element);
+        this.removeAt(index);
     }
     
     indexOf(element){
+        for (let index = 0; index < this.data.length; index++)
+            if(element===this.data[index]) 
+                return index;
+        return -1;
     }
     
     isEmpty(){
+        return this.size()===0;
     }
     
     size(){
+        return this.data.length;
     }
     
     getElement(position){
+        if (position >= 0 && position < this.size())
+            return this.data[position];
+        return null;
     }
 
     search(value){
+        return this.data.some((n)=> n===value)
     }
 }
