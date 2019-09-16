f2af56b4cf21acdad21b8d15e730a659c8a0e4f7
samuelclerod <samuelclerod@gmail.com>
2019-09-16 20:10:03 -0300

    Concluida a implementação do arraylist

--- js/arraylist.js
+++ js/arraylist.js
@@ -6,39 +6,51 @@ class ArrayList{
     }
 
     append(element) {
-
+        this.data.push(element);
     }
 
     insert(position, element) {
-
+        if(this.positionIsValid(position) || position==this.size()){
+            this.data.splice(position, 0, element);
+        }
+    }
+    positionIsValid(position){
+        return (position >= 0 && position < this.size());
     }
 
     remove(element) {
-
+        let index = this.search(element);
+        if(index!=-1) return this.data.splice(index, 1);
     }
 
     removeAt(position){
-
+        if(this.positionIsValid(position)){
+            this.data.splice(position, 1);
+        }
     }
 
     size(){
-
+        return this.data.length;
     }
 
     show(separator='-'){
-
+        return this.data.join(separator);
     }
 
     isEmpty(){
-
+        return this.data.length===0;
     }
 
     getElement(position){
-
+        if(this.positionIsValid(position))
+            return this.data[position];
+        return null;
     }
 
     search(element){
-
+        for(let i =0 ; i<this.data.length; i++)
+            if(this.data[i]==element) return i;
+        return -1;
     }
 
 
