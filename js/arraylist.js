536434ae35fb001f11c006772bad9665a5772ad3
Samuel Rodrigues <samuelclerod@gmail.com>
2020-03-31 20:06:47 -0300

    implementado lista estática

--- js/arraylist.js
+++ js/arraylist.js
@@ -1,31 +1,45 @@
 
-class ArrayList{
-    constructor(){
-
+class ArrayList {
+    constructor() {
+        this.data = []
     }
 
-    append(value){
-
+    append(value) {
+        this.data.push(value)
     }
 
-    insert(position, value){
-
+    insert(position, value) {
+        if (position >= 0 && position <= this.data.length) {
+            this.data.splice(position, 0, value)
+        }
     }
 
-    remove(value){
-
+    remove(value) {
+        const index = this.indexOf(value)
+        this.removeAt(index)
     }
 
-    removeAt(position){
-
+    indexOf(value) {
+        for (let i = 0; i < this.size(); i++) {
+            if (this.data[i] == value) {
+                return i
+            }
+        }
+        return -1
     }
 
-    size(){
-
+    removeAt(position) {
+        if (position >= 0 && position < this.data.length) {
+            this.data.splice(position, 1)
+        }
     }
 
-    toString(separator='-'){
-        
+    size() {
+        return this.data.length
+    }
+
+    toString(separator = '-') {
+        return this.data.join(separator)
     }
 
 }
---
+++ js/linkedlist.js
@@ -0,0 +1,27 @@
+
+class LinkedList {
+    constructor() {
+    }
+
+    append(value) {
+    }
+
+    insert(position, value) {
+    }
+
+    remove(value) {
+
+    }
+
+    removeAt(position) {
+
+    }
+
+    size() {
+
+    }
+
+    toString(separator = '-') {
+    }
+
+}
