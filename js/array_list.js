67cb01c8e01471ba64b4e02cb0eae27fbca6ee05
Samuel Rodrigues <samuelclerod@gmail.com>
2020-09-29 19:50:54 -0300

    projeto inicial de EDLIST20202

---
+++ js/array_list.js
@@ -0,0 +1,31 @@
+class ArrayList {
+
+    constructor() {
+
+    }
+
+    append(value) {
+
+    }
+
+    insert(position, value) {
+
+    }
+
+    remove(value) {
+
+    }
+
+    removeAt(position) {
+
+    }
+
+    size() {
+
+    }
+
+    toString(separator = '-') {
+
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-var list;
+var list = new ArrayList();
 
-$().ready(function(){
+$().ready(function () {
     $('#insert').click(insertElement)
     $('#insert_at').click(insertElementAt)
     $('#remove').click(removeElement)
     $('#remove_at').click(removeElementAt)
 })
 
-function showData(){
+function showData() {
     let text = `<div class="ui label">
                     ${list.toString('</div><div class="ui label">')}
                 </div>`
@@ -15,23 +15,23 @@ function showData(){
     out.empty()
     out.append(text)
 }
-function insertElement(){
+function insertElement() {
     let val = prompt('digite um valor a ser inserido:')
     list.append(val)
     showData();
 }
-function insertElementAt(){
+function insertElementAt() {
     let val = prompt('digite um valor a ser inserido:')
     let pos = prompt('digite uma posição a inserir:')
     list.insert(pos, val)
     showData();
 }
-function removeElement(){
+function removeElement() {
     let val = prompt('digite um valor a ser removido:')
     list.remove(val)
     showData();
 }
-function removeElementAt(){
+function removeElementAt() {
     let pos = prompt('digite uma posição a remover:')
     list.removeAt(pos)
     showData();
