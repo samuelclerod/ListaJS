0ae9114247378dcdc983984d0049963ba1173e7a
Samuel Rodrigues <samuelclerod@gmail.com>
2021-03-30 18:52:55 -0300

    adicionado estrutura básica do linkedlist

---
+++ js/linkedlist.js
@@ -0,0 +1,38 @@
+
+class LinkedList {
+
+  constructor() {
+  }
+
+  append(value) {
+
+  }
+
+  insert(position, value) {
+
+  }
+
+  remove(value) {
+
+  }
+
+  removeAt(position) {
+
+  }
+
+  size() {
+
+  }
+
+  toString(separator = '-') {
+  }
+
+}
+
+
+class Node {
+  constructor(value) {
+    this.content = value;
+    this.next = null;
+  }
+}
