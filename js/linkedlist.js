8d28015c36711d998166ea1092f18bafb06faf04
Samuel Rodrigues <samuelclerod@gmail.com>
2021-03-30 19:46:10 -0300

    implementado append e toString da lista encadeada

--- js/linkedlist.js
+++ js/linkedlist.js
@@ -1,11 +1,26 @@
-
 class LinkedList {
 
   constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  _lastNode() {
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    return current;
   }
 
   append(value) {
-
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this._lastNode().next = newNode;
+    }
+    this.length++;
   }
 
   insert(position, value) {
@@ -21,18 +36,17 @@ class LinkedList {
   }
 
   size() {
-
+    return this.length;
   }
 
   toString(separator = '-') {
+    let output = '';
+    let current = this.head;
+    while (current !== null) {
+      output += current.content + separator;
+      current = current.next;
+    }
+    return output.substring(0, (output.length - separator.length));
   }
 
-}
-
-
-class Node {
-  constructor(value) {
-    this.content = value;
-    this.next = null;
-  }
 }
---
+++ js/node.js
@@ -0,0 +1,6 @@
+class Node {
+  constructor(value) {
+    this.content = value;
+    this.next = null;
+  }
+}
