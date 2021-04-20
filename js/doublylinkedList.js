049a6a7a11f553f8e034e452ef0f8c558c25854b
Samuel Rodrigues <samuelclerod@gmail.com>
2021-04-20 19:40:48 -0300

    terminada lista duplamente encadeada

--- js/doublylinkedList.js
+++ js/doublylinkedList.js
@@ -5,33 +5,6 @@ class DoublylinkedList {
     this.length = 0;
   }
 
-  _lastNode() {
-    if (!this.head)
-      return null
-    let current = this.head;
-    while (current.next !== null) {
-      current = current.next;
-    }
-    return current;
-  }
-
-  _getNodeByIndex(index) {
-    let current = this.head, i = 0;
-    while (i != index) {
-      i++;
-      current = current.next;
-    }
-    return current;
-  }
-
-  _getNodeByValue(value) {
-    let current = this.head;
-    while (current && value != current.content) {
-      current = current.next;
-    }
-    return current;
-  }
-
   append(value) {
     let newNode = new Node(value);
     const lastNode = this._lastNode();
@@ -75,11 +48,23 @@ class DoublylinkedList {
   }
 
   remove(value) {
-
+    const selected = this._getNodeByValue(value);
+    if (!selected) {
+      throw new Error('Valor não encontrado!');
+    }
+    return this._removeNode(selected);
   }
 
   removeAt(position) {
-
+    const index = position - 1;
+    if (this.size() === 0) {
+      throw new Error('Lista vazia');
+    }
+    if (index < 0 || index >= this.length) {
+      throw new Error('Posição inválida');
+    }
+    const selected = this._getNodeByIndex(index);
+    return this._removeNode(selected);
   }
 
   size() {
@@ -94,6 +79,56 @@ class DoublylinkedList {
       current = current.next;
     }
     return output.substring(0, (output.length - separator.length));
+  }
+
+  //private methods
+
+  _lastNode() {
+    if (!this.head)
+      return null
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  _getNodeByIndex(index) {
+    let current = this.head, i = 0;
+    while (i != index) {
+      i++;
+      current = current.next;
+    }
+    return current;
+  }
+
+  _getNodeByValue(value) {
+    let current = this.head;
+    while (current && value != current.content) {
+      current = current.next;
+    }
+    return current;
+  }
+  _removeNode(selected) {
+    if (selected.previous == selected.next) {
+      this.head = null;
+    } else if (!selected.previous) {
+      this.head = selected.next;
+      selected.next = null;
+      this.head.previous = null;
+    } else if (!selected.next) {
+      selected.previous.next = null;
+      selected.previous = null;
+    } else {
+      selected.previous.next = null;
+      selected.next.previous = null;
+      selected.previous.next = selected.next;
+      selected.next.previous = selected.previous;
+      selected.next = null;
+      selected.previous = null;
+    }
+    this.length--;
+    return selected.content;
   }
 
 }
