6a97a9abed47584c33be4b6c6132c8557cd7dd55
Samuel Rodrigues <samuelclerod@gmail.com>
2023-11-23 21:45:02 -0300

    added remote on dynamic list

--- js/dynamic_list.js
+++ js/dynamic_list.js
@@ -27,9 +27,62 @@ class DynamicList {
     this.length++
   }
 
-  insert(position, value) {}
+  insert(position, value) {
+    if (position < 0 || position > this.size()) {
+      throw new Error('Invalid position')
+    }
 
-  remove(value) {}
+    if (position == this.length) {
+      this.append(value)
+      return
+    }
+
+    const newNode = new Node(value)
+
+    if (position == 0) {
+      newNode.next = this.head
+      this.head = newNode
+    } else {
+      let previous = this.head
+      let current = previous.next
+      let index = 1
+      while (index != position) {
+        index++
+        previous = current
+        current = current.next
+      }
+      previous.next = newNode
+      newNode.next = current
+    }
+
+    this.length++
+  }
+
+  remove(value) {
+    let current = this.head
+    let previous = null
+
+    while (current != null) {
+      if (current.content === value) {
+        break
+      }
+      previous = current
+      current = current.next
+    }
+    if (current == null) {
+      return null
+    }
+
+    if (current == this.head) {
+      this.head = current.next
+    } else {
+      previous.next = current.next
+    }
+    current.next = null
+    this.length--
+
+    return current.content
+  }
 
   removeAt(position) {}
 
